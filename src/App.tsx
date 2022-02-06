9da0e5a8cfbe2f54eea0c6e154d536d5fc681d4d
Maroš Hajník <hajnik.work@gmail.com>
2022-02-06 20:47:33 +0100

    Created heading of application. Updated app component markup and styles. Changed css file to sass file. Included nice handwriting font family "Kalam". Installed node-sass as dev dependency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <h1>Hello world!</h1>
-  );
-}
+const App: React.FC = () => {
+	return (
+		<main className='taskdone'>
+			<header>
+				<h1>Task Done</h1>
+			</header>
+		</main>
+	);
+};
 
 export default App;
