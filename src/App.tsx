2c54c58a69097d3c4f3f82cd7f8094fab0a48c16
Maroš Hajník <hajnik.work@gmail.com>
2022-02-07 08:55:50 +0100

    Created state for todos in App component. Created props for inputField component. Created Todo interface in models folder.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 // Reusable components
 import InputField from './components/InputField';
 
+// Models
+import { Todo } from './models/Todo';
+
 const App: React.FC = () => {
+	const [todo, setTodo] = useState<string>('');
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	const AddHandler = (e: React.FormEvent) => {
+		e.preventDefault();
+		if (todo) {
+			setTodos([...todos, { id: Date.now(), todo: todo.trim(), isDone: false }]);
+			setTodo('');
+		}
+	};
+
 	return (
 		<>
 			<header>
 				<h1>Task Done</h1>
 			</header>
 			<main className='taskdone'>
-				<InputField />
+				<InputField todo={todo} setTodo={setTodo} onAdd={AddHandler} />
 			</main>
 		</>
 	);
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 // styles
 import './InputField.scss';
-const InputField = () => {
+
+interface Props {
+	todo?: string;
+	setTodo: React.Dispatch<React.SetStateAction<string>>;
+	onAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, onAdd }) => {
 	return (
-		<form className='input'>
-			<input type='input' placeholder='Enter a task' className='input__box' />
+		<form className='input' onSubmit={onAdd}>
+			<input type='input' value={todo} placeholder='Enter a task' className='input__box' onChange={(e) => setTodo(e.target.value)} />
 			<button className='input__submit' type='submit'>
 				Add
 			</button>
